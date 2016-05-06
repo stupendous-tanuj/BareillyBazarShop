ca420a3556735448468dd3f276628f6f4b7203b4
Tanuj <stupendous.tanuj@gmail.com>
2016-05-06 15:29:28 +0530

    Create Fetch_Product_Requests.php

---
+++ Fetch_Product_Requests.php
@@ -0,0 +1,65 @@
+<?php
+/*
+{"applicationId": "ANDSH1001","userId": "9503157441"}
+ShopTheFortune/Fetch_Associated_DeliveryPersons.php?input={"applicationId": "ANDSH1001","userId": "9503157441"}
+*/
+include 'database.php';
+header('Content-type: x-www-form-urlencoded');
+$jsonRequest = json_decode( processRequest($_POST['input']), true);
+$applicationId = mysqli_real_escape_string($link,$jsonRequest['applicationId']);
+$userId = mysqli_real_escape_string($link,$jsonRequest['userId']); 
+
+try {
+if($applicationId=="")
+throw new Exception($applicationId_MISSING);
+
+if($userId=="")
+throw new Exception($userId_MISSING);
+
+
+		$query = "SELECT CustomerMobileNumber,CustomerName, ProductDescription, ProductRequest.CreationTime FROM `ProductRequest`,Customer WHERE (ProductRequest.CustomerMobileNumber = Customer.CustomerMobileNumber)";
+	
+	$result=mysqli_query($link,$query);
+	$dataResult = array();
+	$dataResult['status']=1;
+	$i = 0;
+	if(mysqli_num_rows($result) >= 1){
+	while($row=mysqli_fetch_array($result)){
+		
+			$dataRow = array();
+			$dataRow['customerMobileNumber']=$row['CustomerMobileNumber']." ".$row['CustomerName'];
+			$dataRow['productDescription']=$row['ProductDescription'];
+			$dataRow['creationTime']=$row['CreationTime'];
+			$dataResult['response']['productRequest'][$i++]=$dataRow;
+		}
+	}
+else if(mysqli_num_rows($result) == 0){
+
+	$dataResult = array();
+	$dataRow = array();
+	$dataResult['status']=1;
+	$dataResult['response']['productRequest']=$dataRow;
+}
+else{
+throw new Exception($NO_RECORD_FOUND);
+}
+	
+	echo str_replace('\\', '', processResponse($jsonRequest,json_encode($dataResult))); 
+}
+
+//catch exception
+catch(Exception $e) {
+	$data = array();
+	$data['status']=0;
+	$data['response']['errorCode']=$FETCH_PRODUCT_REQUESTS_SERVICE_ERROR_CODE;
+	$data['response']['errorMessage']=$e->getMessage();
+	$data['response']['supportContactNumber']=fetchSupportContactNumber($link,$applicationId);
+	logAnError($link,'Fetch_Product_Requests', $_POST, $userId, json_encode($data), $e);
+    echo json_encode($data); 
+}
+
+finally {
+	mysqli_close($link);
+}
+
+?>
