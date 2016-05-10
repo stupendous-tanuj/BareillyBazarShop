b15a723112c9571bf5e4f1ead729c373d7d63b75
Tanuj <stupendous.tanuj@gmail.com>
2016-05-10 14:32:11 +0530

    Create Apply_Promocode.php

---
+++ Apply_Promocode.php
@@ -0,0 +1,9 @@
+Check the validity of the Promo Code.
+Evaluate Eligibility Criteria.
+Evaluate Code Use Frequency.
+Evaluate Minimum order value.
+Calculate Discount value.
+in case of Flat Discount
+In case of Percent Discount.
+Normalize the discount according to maximum discount value.
+
